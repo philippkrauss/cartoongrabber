4d684ae6d9d1b5d06b23a34b5be8d9d4804056cb
Philipp Krauss <philipp.krauss@codecentric.de>
2017-08-03 11:47:48 +0200

    Bugfix: Jedis connections are not closed

--- cartoongrabber-web/src/main/java/cartoongrabber/web/controller/CartoonController.java
+++ cartoongrabber-web/src/main/java/cartoongrabber/web/controller/CartoonController.java
@@ -3,6 +3,8 @@ package cartoongrabber.web.controller;
 import cartoongrabber.model.CartoonStrip;
 import cartoongrabber.web.model.CartoonStateMap;
 import cartoongrabber.web.service.CartoonPersistenceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,8 @@ public class CartoonController {
 
     private final CartoonPersistenceService persistenceService;
 
+    private final Logger log = LoggerFactory.getLogger(CartoonController.class);
+
     @Autowired
     public CartoonController(CartoonPersistenceService persistenceService) {
         this.persistenceService = persistenceService;
@@ -24,6 +28,7 @@ public class CartoonController {
 
     @RequestMapping("/cartoons")
     public String getCartoons(@RequestParam(value = "date", required = false) String date, Model model) {
+        log.info("get cartoons for date [{}]", date);
         SortedSet<LocalDate> availableDates = new TreeSet<>(persistenceService.getDates());
         LocalDate selectedDate = getSelectedDate(availableDates, date);
         List<CartoonStrip> cartoons = persistenceService.getCartoonsForDate(selectedDate);
@@ -32,6 +37,7 @@ public class CartoonController {
         model.addAttribute("cartoons", cartoons);
         model.addAttribute("date", selectedDate);
         model.addAttribute("isOldMap", isOldMap);
+        log.debug("get cartoons done, found [{}]", cartoons.size());
         return "cartoons";
     }
 
@@ -56,8 +62,10 @@ public class CartoonController {
 
     @RequestMapping("/dates")
     public String getList(Model model) {
+        log.info("get dates");
         Collection<LocalDate> dates = persistenceService.getDates();
         model.addAttribute("dates", dates);
+        log.debug("get dates done, found [{}]", dates.size());
         return "dates";
     }
 }
--- cartoongrabber-web/src/main/java/cartoongrabber/web/service/RedisPersistenceService.java
+++ cartoongrabber-web/src/main/java/cartoongrabber/web/service/RedisPersistenceService.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -27,14 +28,18 @@ public class RedisPersistenceService implements CartoonPersistenceService {
 
     @Override
     public Collection<LocalDate> getDates() {
-        RedisStore store = new RedisStore(jedisFactory.createJedis());
-        return store.readDates();
+        try(Jedis jedis = jedisFactory.createJedis()) {
+            RedisStore store = new RedisStore(jedis);
+            return store.readDates();
+        }
     }
 
     @Override
     public List<CartoonStrip> getCartoonsForDate(LocalDate date) {
-        log.debug("Getting cartoons for date [{}]", date);
-        RedisStore store = new RedisStore(jedisFactory.createJedis());
-        return store.readCartoonsForDate(date);
+        try(Jedis jedis = jedisFactory.createJedis()) {
+            log.debug("Getting cartoons for date [{}]", date);
+            RedisStore store = new RedisStore(jedis);
+            return store.readCartoonsForDate(date);
+        }
     }
 }
